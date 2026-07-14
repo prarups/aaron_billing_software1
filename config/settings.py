36b49bdda557664655c22ac73e01120c9c0f4263
prarups <paulpraveen328@gmail.com>
2026-07-14 11:21:14 +0530

    build error fixed

--- config/settings.py
+++ config/settings.py
@@ -56,8 +56,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     
     # Third party
     'rest_framework',
